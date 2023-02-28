a7a472208d0a1f30a95b0f8c845cbeeae89c83ad
Dmitriy Angve <kirirch129@gmail.com>
2023-02-28 07:31:36 +0300

    #

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -2,7 +2,7 @@ function ErrorPage() {
   return <h1>An error occured!</h1>;
 }
 
-export default Error;
+export default ErrorPage;
 
 // 292. ERROR HANDLING WITH CUSTOMS ERRORS
 // CAME FROM Events.js
